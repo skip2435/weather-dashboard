223180deb0dddf1c3e6566877d80018ace17b1e1
skip2435 <skip2434@gmail.com>
2023-05-17 13:32:02 -0400

    Added the Map and icons to the hourly forecast.

--- weather-app/resources/js/components/HourlyWeatherUI.vue
+++ weather-app/resources/js/components/HourlyWeatherUI.vue
@@ -9,13 +9,15 @@
       </button>
     </div>
 
-    <div class="grid grid-cols-5 gap-4" v-if="forecast">
+    <div id="map" style="width: 100%; height: 400px"></div>
+
+    <div class="grid grid-cols-5 gap-4 mt-4" v-if="forecast">
       <div class="hour-forecast transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" v-for="(hour, index) in forecast" :key="index">
-        <div class="card bg-blue-100 p-6 rounded-lg shadow-2xl">
-          <h2 class="text-2xl font-bold mb-2">{{hour.time}}</h2>
+        <div class="card bg-white p-6 rounded-lg shadow-2xl">
+          <h2 class="text-2xl font-bold mb-2">{{ new Date(hour.time).toLocaleTimeString() }}</h2>
 
           <div class="weather-info flex items-center justify-center">
-            <i class="wi wi-day-sunny text-6xl"></i> <!-- Replace with appropriate class -->
+            <font-awesome-icon :icon="['fas', getWeatherIcon(hour.conditions)]" class="text-6xl"/>
             <div class="ml-4">
               <h3 class="text-lg font-semibold">Temperature:</h3>
               <p class="text-xl font-semibold">{{hour.temp}}°C</p>
@@ -38,26 +40,71 @@
 
 <script>
 import axios from 'axios';
+import mapboxgl from 'mapbox-gl';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+const mapboxAccessToken = import.meta.env.VITE_VUE_APP_MAPBOX_ACCESS_TOKEN;
 
 export default {
   name: "HourlyWeatherUI",
+  components: {
+    FontAwesomeIcon
+  },
   data() {
     return {
       city: '',
-      forecast: null
+      forecast: null,
+      lon: null,
+      lat: null
     };
   },
   methods: {
+    getWeatherIcon(condition) {
+      condition = condition.toLowerCase();
+      if (condition.includes('clear')) {
+        return 'sun';
+      } else if (condition.includes('rain')) {
+        return 'cloud-rain';
+      } else if (condition.includes('cloudy')) {
+        return 'cloud';
+      } else if (condition.includes('snow')) {
+        return 'snowflake';
+      } else if (condition.includes('overcast')) {
+        return 'cloud';
+      } else {
+        return 'question-circle'; // Fallback icon
+      }
+    },
     async getForecast() {
       const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.city}?key=${import.meta.env.VITE_VISUAL_CROSSING_API_KEY}`);
-
+    
       this.forecast = response.data.days[0].hours.map(hour => ({
-        time: hour.datetime,
+        time: new Date('1970-01-01T' + hour.datetime + 'Z'),
         temp: hour.temp,
         feelsLike: hour.feelslike,
         conditions: hour.conditions,
         precipitation: hour.precipprob,
       }));
+    
+      const mapboxResponse = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.city}.json?access_token=${mapboxAccessToken}`);
+      if (mapboxResponse.data.features.length > 0) {
+        this.lon = mapboxResponse.data.features[0].center[0];
+        this.lat = mapboxResponse.data.features[0].center[1];
+      }
+      this.initMap();
+    },
+    initMap() {
+      mapboxgl.accessToken = mapboxAccessToken;
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [this.lon, this.lat], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
     }
   }
 };
